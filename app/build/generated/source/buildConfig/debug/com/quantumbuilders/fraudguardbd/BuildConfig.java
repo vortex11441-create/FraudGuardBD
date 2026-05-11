ac85aa6d53bcb449fa4dbeacfcea2056e5f63bac
vortex11441-create <vortex11441@gmail.com>
2026-05-11 19:39:38 +0600

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/quantumbuilders/fraudguardbd/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.quantumbuilders.fraudguardbd;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.quantumbuilders.fraudguardbd";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String OPENROUTER_API_KEY = "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";
+}
